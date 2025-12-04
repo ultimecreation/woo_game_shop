2c711578d1fcfb75446934de98660a029a75359e
ultimecreation <you@example.comecho>
2025-12-04 16:38:58 +0100

    header completed

---
+++ plugins/blocks-woo-game-shop/build/block-header/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '8b7f5c1bdd6911e8df31');
---
+++ plugins/blocks-woo-game-shop/build/block-header/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '74029589aad345278868');
